9611d3d65c17f5d9dd1facc8a5aa09e86d066793
TellMeT <santiagoocardona801@gmail.com>
2026-06-22 21:12:10 -0500

    actualizacion barra en scroll infinito

--- widgets_app/lib/presentation/screens/infinite_scroll/infinite_scroll_screen.dart
+++ widgets_app/lib/presentation/screens/infinite_scroll/infinite_scroll_screen.dart
@@ -1,6 +1,7 @@
 import 'package:animate_do/animate_do.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:flutter/services.dart';
 
 class InfiniteScrollScreen extends StatefulWidget {
 
@@ -22,7 +23,6 @@ class _InfiniteScrollScreenState extends State<InfiniteScrollScreen> {
   @override
   void initState() {
 
-    // TODO: implement initState
     super.initState();
 
     scrollController.addListener((){
@@ -108,40 +108,43 @@ class _InfiniteScrollScreenState extends State<InfiniteScrollScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.black,
-      body: MediaQuery.removePadding(
-        context: context,
-        removeTop: true,
-        removeBottom: true,
-        child: RefreshIndicator(
-          onRefresh: onRefresh,
-          child: ListView.builder(
-            controller: scrollController,
-            itemCount: imagesIds.length,
-            itemBuilder: (context, index) {
-              return FadeInImage(
-                fit: BoxFit.cover,
-                width: double.infinity,
-                height: 300,
-                placeholder: AssetImage('assets/images/jar-loading.gif'), 
-                image: NetworkImage('https://picsum.photos/id/${imagesIds[index]}/500/300'),
-              );
-            },
+    return AnnotatedRegion<SystemUiOverlayStyle>(
+      value: SystemUiOverlayStyle.light,
+      child: Scaffold(
+        backgroundColor: Colors.black,
+        body: MediaQuery.removePadding(
+          context: context,
+          removeTop: true,
+          removeBottom: true,
+          child: RefreshIndicator(
+            onRefresh: onRefresh,
+            child: ListView.builder(
+              controller: scrollController,
+              itemCount: imagesIds.length,
+              itemBuilder: (context, index) {
+                return FadeInImage(
+                  fit: BoxFit.cover,
+                  width: double.infinity,
+                  height: 300,
+                  placeholder: AssetImage('assets/images/jar-loading.gif'), 
+                  image: NetworkImage('https://picsum.photos/id/${imagesIds[index]}/500/300'),
+                );
+              },
+            ),
           ),
         ),
-      ),
-
-      floatingActionButton: FloatingActionButton(
-        onPressed: () => context.pop(),
-        child: 
-            isLoading ?
-            SpinPerfect(
-            infinite: true,
-            child: Icon(Icons.refresh_rounded
-          )
-        ): FadeIn(child: Icon(Icons.arrow_back_ios_new_outlined))
-
+      
+        floatingActionButton: FloatingActionButton(
+          onPressed: () => context.pop(),
+          child: 
+              isLoading ?
+              SpinPerfect(
+              infinite: true,
+              child: Icon(Icons.refresh_rounded
+            )
+          ): FadeIn(child: Icon(Icons.arrow_back_ios_new_outlined))
+      
+        ),
       ),
     );
   }
